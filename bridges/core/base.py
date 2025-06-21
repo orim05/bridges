89cac0f8f4d7133f34140ec27579aa2fe754eba8
Ori Meiri <orimeiri05@gmail.com>
2025-06-21 11:49:15 +0300

    Add abstract base classes and minimal core types for bridges framework

---
+++ bridges/core/base.py
@@ -0,0 +1,51 @@
+"""
+bridges.core.base
+-----------------
+Abstract base classes and minimal core types for bridges framework.
+"""
+from abc import ABC, abstractmethod
+from typing import Any, Dict
+
+class ParamSource(ABC):
+    """
+    Base class for parameter sources.
+    """
+    pass
+
+class OutputDestination(ABC):
+    """
+    Base class for output destinations.
+    """
+    pass
+
+class IBridge(ABC):
+    """
+    Abstract base class for a Bridge.
+    """
+    pass
+
+
+class BridgeInterface(ABC):
+    """
+    Abstract interface for bridge UIs (CLI, Web, etc).
+
+    :param bridge: An instance of IBridge to be used by the interface.
+    """
+    def __init__(self, bridge: IBridge):
+        self.bridge = bridge
+
+    @abstractmethod
+    def customize(self, config: Dict[str, Any]):
+        """
+        Customize the interface with the given configuration.
+
+        :param config: Dictionary of configuration options.
+        """
+        pass
+
+    @abstractmethod
+    def run(self):
+        """
+        Run the interface.
+        """
+        pass

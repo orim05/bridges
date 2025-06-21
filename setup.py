deec57c89bbbeeccd72c5f05ca6036803b08cd42
Ori Meiri <orimeiri05@gmail.com>
2025-06-21 16:34:41 +0300

    Update version to 0.1.1 in setup.py and modify release workflow to upload build artifacts instead of publishing to PyPI.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="bridges",
-    version="0.1.0",
+    version="0.1.1",
     description="A framework-agnostic middleware for automatic UI generation.",
     author="orim05",
     author_email="orimeiri18@email.com",
